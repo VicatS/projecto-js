c335a342506a27e32b9df7a461b646bb1171931d
victor <victorcs2704@gmail.com>
2020-06-16 17:59:22 -0400

    133. Cambiador de temas del sitio web

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(function () {
+
+    //Slider
     $('.gallery').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200,
         responsive: true,
     });
+
+//    Selector of theme
+    let theme = $("#theme")
+
+    $("#to-green").on("click", function () {
+        theme.attr("href", "css/green.css")
+    })
+
+    $("#to-red").on("click", function () {
+        theme.attr("href", "css/red.css")
+    })
+
+    $("#to-blue").on("click", function () {
+        theme.attr("href", "css/blue.css")
+    })
+
 })
