5c8e9f1334d4747356af8c5bf5327138b0df633e
victor <victorcs2704@gmail.com>
2020-06-16 18:02:49 -0400

    132. Listado dinamico de articulos/posts

--- js/main.js
+++ js/main.js
@@ -8,6 +8,76 @@ $(function () {
         responsive: true,
     });
 
+//    Posts
+
+    let posts = [
+        {
+            title: 'Proof of title 1',
+            date: 'Published in ' + moment().format("LLLL"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+        },
+        {
+            title: 'Proof of title 2',
+            date: 'Published in ' + moment().format("LLLL"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+        },
+        {
+            title: 'Proof of title 3',
+            date: 'Published in ' + moment().format("LLLL"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+        },
+        {
+            title: 'Proof of title 4',
+            date: 'Published in ' + moment().format("LLLL"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+        },
+        {
+            title: 'Proof of title 5',
+            date: 'Published in ' + moment().format("LLLL"),
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+        }
+    ]
+
+    posts.forEach((item, index) => {
+        let post = `
+          <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>
+                    ${item.content}
+                </p>
+                <a href="#" class="button-more">Read more</a>
+          </article>
+        `
+
+        $("#posts").append(post)
+    })
+
 //    Selector of theme
     let theme = $("#theme")
 
