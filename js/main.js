d933afc9e86916549f4884a8f6b7a13120421a1a
victor <victorcs2704@gmail.com>
2020-06-17 17:27:12 -0400

    136. Acordeon

--- js/main.js
+++ js/main.js
@@ -1,70 +1,72 @@
 $(function () {
 
     //Slider
-    $('.gallery').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true,
-    });
+    if (window.location.href.indexOf('index') > -1) {
+        $('.gallery').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true,
+        });
+    }
 
 //    Posts
+    if (window.location.href.indexOf('index') > -1) {
+        let posts = [
+            {
+                title: 'Proof of title 1',
+                date: 'Published in ' + moment().format("LLLL"),
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                    '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                    '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                    '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                    '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                    '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+            },
+            {
+                title: 'Proof of title 2',
+                date: 'Published in ' + moment().format("LLLL"),
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                    '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                    '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                    '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                    '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                    '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+            },
+            {
+                title: 'Proof of title 3',
+                date: 'Published in ' + moment().format("LLLL"),
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                    '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                    '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                    '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                    '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                    '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+            },
+            {
+                title: 'Proof of title 4',
+                date: 'Published in ' + moment().format("LLLL"),
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                    '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                    '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                    '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                    '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                    '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+            },
+            {
+                title: 'Proof of title 5',
+                date: 'Published in ' + moment().format("LLLL"),
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
+                    '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
+                    '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
+                    '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
+                    '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
+                    '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
+            }
+        ]
 
-    let posts = [
-        {
-            title: 'Proof of title 1',
-            date: 'Published in ' + moment().format("LLLL"),
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
-                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
-                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
-                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
-                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
-                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
-        },
-        {
-            title: 'Proof of title 2',
-            date: 'Published in ' + moment().format("LLLL"),
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
-                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
-                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
-                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
-                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
-                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
-        },
-        {
-            title: 'Proof of title 3',
-            date: 'Published in ' + moment().format("LLLL"),
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
-                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
-                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
-                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
-                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
-                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
-        },
-        {
-            title: 'Proof of title 4',
-            date: 'Published in ' + moment().format("LLLL"),
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
-                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
-                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
-                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
-                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
-                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
-        },
-        {
-            title: 'Proof of title 5',
-            date: 'Published in ' + moment().format("LLLL"),
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut volutpat sem. Curabitur faucibus\n' +
-                '                    sit amet ante ut malesuada. Pellentesque a sagittis neque, at posuere nisi. Ut a ligula venenatis,\n' +
-                '                    molestie augue vitae, congue turpis. Nullam semper felis sed semper efficitur. Phasellus velit nisi,\n' +
-                '                    malesuada vitae lobortis vitae, gravida sit amet augue. Praesent pulvinar tempor orci, eget commodo\n' +
-                '                    justo iaculis vitae. Morbi ultricies, est ut imperdiet ultrices, velit tellus pretium neque, in\n' +
-                '                    finibus quam metus mollis nibh. Etiam a arcu egestas, porttitor augue a, maximus ipsum.'
-        }
-    ]
-
-    posts.forEach((item, index) => {
-        let post = `
+        posts.forEach((item, index) => {
+            let post = `
           <article class="post">
                 <h2>${item.title}</h2>
                 <span class="date">${item.date}</span>
@@ -75,8 +77,9 @@ $(function () {
           </article>
         `
 
-        $("#posts").append(post)
-    })
+            $("#posts").append(post)
+        })
+    }
 
 //    Selector of theme
     let theme = $("#theme")
@@ -125,4 +128,8 @@ $(function () {
         })
     }
 
+    // show in about.html
+    if (window.location.href.indexOf('about') > -1) {
+        $("#accordion").accordion()
+    }
 })
